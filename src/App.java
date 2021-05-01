6a6d64030c1c00c3cc610d94e05b9ceeae8fdb1a
RafaelEscobar <rafael031015@gmail.com>
2021-05-01 10:38:11 -0500

    Subiendo archivos

---
+++ src/App.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Metodo String");
+
+        //Metodo length
+        String HOL = "ADSI";
+        System.out.println("La cantidad de caracteres de la cadena es: "+HOL.length());
+
+        //Substring
+        System.out.println("El caracter de la posición 1 es: "+HOL.substring(1,2));
+
+        //toLowerCase
+        String cad = "Hola me llamo Rafael Escobar";
+        System.out.println("La frase en minúsculas es: "+cad.toLowerCase()+", y es mayúsculas es: "+cad.toUpperCase());
+
+        //equalsIgnoreCase
+        String cad1 = "Hola me llamo Rafael Escobar";
+        System.out.println("Son iguales los valores? "+cad.equalsIgnoreCase(cad1));
+
+        // replaceAll
+        String cad2 = "Hola, mi nombre es Rafael Escobar";
+        System.out.println("El texto a reemplazar es Rafael "+cad2.replaceAll("Rafael", "Eduardo"));
+
+        //String a int
+
+        String let = "45";
+        int num = 23;
+        int num1 = Integer.parseInt(let);
+        System.out.println("La suma de los dos valores es: "+(num+num1));
+
+        //pedir datos
+        Scanner edad = new Scanner(System.in);
+        System.out.println("Ingrese su edad por favor: ")
+        String nueva = edad.nextLine();
+        System.out.println("Su edad es: "+nueva);
+
+        
+        
+    }
+}
---
+++ src/Ejercicio1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Ejercicio1 {
+    public static void main(String[] args) throws Exception{
+        //Pedimos el nombre
+        System.out.println("Por favor ingrese su nombre: ");
+
+        //Lo guardamos
+        Scanner nombre = new Scanner(System.in);
+        String nombrein = nombre.nextLine();
+        nombre.close();
+
+        //Declaramos variables para comparar y así evitamos problemas con mayúsculas
+        String prima = "A";
+        String segun = "E";
+        String terc = "I";
+        String cuart = "O";
+        String quint = "U";
+
+        //Lo análizamos con condicionales
+        //Si el nombre comienza con A o con E mostramos el nombre en consola
+        if (nombrein.substring.toLowerCase(1, 1) == prima || nombrein.substring.equalsIgnoreCase(1, 1) == segun){
+            System.out.println("Su nombre es: "+nombrein);
+        }
+        else if(nombrein.substring(1, 1) == "I" || nombrein.substring(1, 1) == "O"){
+
+        }
+
+    }
+}
